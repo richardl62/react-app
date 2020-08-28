66b19482e804332450390e06f36a9c8386d83cc7
slightly-nerdy <github@slightlynerdy.org>
2020-08-28 18:48:34 +0100

    Updates

---
+++ precache-manifest.1524c13a1a583f21c0f6399d1e1b0aa0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b1e5851ad0f6800d04751f60b2b2533",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "c4ce67e8dca20dcc43ef",
+    "url": "/react-app/static/css/main.9447384d.chunk.css"
+  },
+  {
+    "revision": "5d91e551ab88527533f0",
+    "url": "/react-app/static/js/2.38255440.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app/static/js/2.38255440.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4ce67e8dca20dcc43ef",
+    "url": "/react-app/static/js/main.98301795.chunk.js"
+  },
+  {
+    "revision": "41650333ac72d200b59f",
+    "url": "/react-app/static/js/runtime-main.fa69c5e5.js"
+  }
+]);
