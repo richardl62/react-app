05b228932f5db1574066e71b29c52d7a653c2d0a
slightly-nerdy <github@slightlynerdy.org>
2020-08-28 18:52:30 +0100

    First commit after deployment

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function greet(name) {
+  return <div> Hello, {name} </div>
+}
+class App extends React.Component {
+  render() {
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    const people = ['Jill', 'Jamie', 'Bobby' ];
+
+    return (
+      <div className="App">
+        {people.map(name => greet(name))}
+      </div>
+    );
+  } 
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
