ef820d7686dffd47a558546c5bd0e9574ae513f7
slightly-nerdy <github@slightlynerdy.org>
2020-09-04 11:45:40 +0100

    Wip on Cards and Hands

--- src/index.js
+++ src/index.js
@@ -1,21 +1,115 @@
+import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import Draggable from 'react-draggable'; // The default
 
-function greet(name) {
-  return <div> Hello, {name} </div>
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { error: null, errorInfo: null };
+  }
+  
+  componentDidCatch(error, errorInfo) {
+    // Catch errors in any components below and re-render with error message
+    this.setState({
+      error: error,
+      errorInfo: errorInfo
+    })
+    // You can also log error messages to an error reporting service here
+  }
+  
+  render() {
+    if (this.state.errorInfo) {
+      // Error path
+      return (
+        <div>
+          <h2>Oops. Something went wrong.</h2>
+          <details style={{ whiteSpace: 'pre-wrap' }}>
+            {this.state.error && this.state.error.toString()}
+            <br />
+            {this.state.errorInfo.componentStack}
+          </details>
+        </div>
+      );
+    }
+    // Normally, just render children
+    return this.props.children;
+  }  
 }
+
+function numberAndSuit(name) {
+  const number = name.slice(0,-1).toUpperCase();
+  const suit = name.slice(-1).toUpperCase();
+
+  return [number, suit];
+}
+
+function isRed(suit) {
+  const redSuits = ["D", "H"];
+  return redSuits.includes(suit.toUpperCase()); 
+
+} 
+class Card extends React.Component {
+
+  render() {
+    const [number, suit] = numberAndSuit(this.props.value);
+    const cardClass = isRed(suit) ? "redCard" : "blackCard";
+
+    return (
+      <div {... addClassName(this.props, cardClass)} > 
+        <div> {number} </div>
+        <div> {suit} </div>
+      </div>);
+  }
+}
+
+function addClassName(props, name) {
+  let new_props = {...props}; // Independent copy
+
+  if(props.className) {
+    new_props.className += " " + name;
+  } else {
+    new_props.className =  name
+  }
+
+  return new_props;
+}
+
+class Hand extends React.Component {
+  render() {
+    const cards = this.props.cards? this.props.cards : [];
+
+    return (
+      <div {... addClassName(this.props, "hand")}>
+        {
+          cards.map(card =>
+            <Draggable key={card} >
+              <Card value={card} />
+            </Draggable>
+          )
+        }
+      </div>);
+  }
+}
+
 class App extends React.Component {
   render() {
 
-    const people = ['Jill', 'Jamie', 'Bobby', 'Dave' ];
+    const hand1 = ['1c', '10d', '3H', 'qs' ];
 
+    const hand2 = ['1d', 'Jc', '4s', 'ks' ];
+    
     return (
-      <div className="App">
-        {people.map(name => greet(name))}
-      </div>
+      <ErrorBoundary>
+        <div className="game">
+          <Hand key="hand1" cards={hand1}/>
+          <Hand key="shared" />
+          <Hand key="hand2" cards={hand2}/>
+        </div>
+      </ErrorBoundary>
     );
   } 
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
